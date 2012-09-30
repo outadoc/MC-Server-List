483f26a65a61003629a0b8d2be045f5d6720a5a2
outadoc <outadoc@me.com>
2012-09-30 10:25:58 +0200

    Removed preferences window (will use a bundle instead)

--- Resources/app.js
+++ Resources/app.js
@@ -8,28 +8,13 @@ var tabgroup = Ti.UI.createTabGroup();
 var win_list = Ti.UI.createWindow({
 	url: 'views/list.js',
 	title: 'Server List',
-	barImage: 'img/menubar.png'
-});
-
-var win_more = Ti.UI.createWindow({
-	url: 'views/more.js',
-	title: 'More',
+	barImage: 'img/menubar.png',
+	tabBarHidden: true
 });
 
 var tab1 = Ti.UI.createTab({
-	window: win_list,
-	title: 'Servers',
-	icon: 'img/controller.png'
-});
-
-var tab2 = Ti.UI.createTab({
-	window: win_more,
-	title: 'Settings',
-	icon: 'img/gear.png',
-	backgroundColor: 'stripped'
+	window: win_list
 });
 
 tabgroup.addTab(tab1);
-tabgroup.addTab(tab2);
-
 tabgroup.open();
--- Resources/views/more.js
+++
@@ -1,1 +0,0 @@
-var win = Ti.UI.currentWindow;
