68543236fb19e46dd4a588f7b4f165143b144366
outadoc <outadoc@me.com>
2012-09-30 10:29:45 +0200

    Put app.js code inside an anonymous function

--- Resources/app.js
+++ Resources/app.js
@@ -1,20 +1,22 @@
-var db = Ti.Database.open('servers');
-db.execute('CREATE TABLE IF NOT EXISTS servers (id INTEGER PRIMARY KEY, name VARCHAR(16) NOT NULL, host VARCHAR(32) NOT NULL, port INTEGER DEFAULT 25565)');
-db.file.setRemoteBackup(true);
-db.close();
+(function() {
+	var db = Ti.Database.open('servers');
+	db.execute('CREATE TABLE IF NOT EXISTS servers (id INTEGER PRIMARY KEY, name VARCHAR(16) NOT NULL, host VARCHAR(32) NOT NULL, port INTEGER DEFAULT 25565)');
+	db.file.setRemoteBackup(true);
+	db.close();
 
-var tabgroup = Ti.UI.createTabGroup();
+	var tabgroup = Ti.UI.createTabGroup();
 
-var win_list = Ti.UI.createWindow({
-	url: 'views/list.js',
-	title: 'Server List',
-	barImage: 'img/menubar.png',
-	tabBarHidden: true
-});
+	var win_list = Ti.UI.createWindow({
+		url: 'views/list.js',
+		title: 'Server List',
+		barImage: 'img/menubar.png',
+		tabBarHidden: true
+	});
 
-var tab1 = Ti.UI.createTab({
-	window: win_list
-});
+	var tab1 = Ti.UI.createTab({
+		window: win_list
+	});
 
-tabgroup.addTab(tab1);
-tabgroup.open();
+	tabgroup.addTab(tab1);
+	tabgroup.open();
+})();
